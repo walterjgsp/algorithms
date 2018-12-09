30852ed7c2d22609a169fe53f57a241d4cb396bc
Walter <walterjgsp@gmail.com>
2018-12-09 14:19:11 -0200

    Simple Binary Search

---
+++ algorithms/binary_search.cpp
@@ -0,0 +1,34 @@
+//Simple binary search in a sorted list.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class BinarySearch{
+public:
+   int search(const vector<int> &arr, int target){
+     int start = 0,end=arr.size()-1;
+
+     while(start<=end){
+       int mid = start+(end-start)/2;
+
+       if(arr[mid]==target)
+        return mid;
+       else if(arr[mid]<target)
+        start = mid + 1;
+       else
+        end = mid -1;
+     }
+
+     return -1;
+   }
+};
+
+int main(){
+  vector<int> arr = {1,2,3,4,6,9,13,14,17,25,27};
+  BinarySearch binarySearch;
+
+  cout<<binarySearch.search(arr,6)<<endl;
+
+  return 0;
+}
