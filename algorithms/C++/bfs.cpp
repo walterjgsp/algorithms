5964488b911a9170487798be1b853d1bdfff77bf
Walter <walterjgsp@gmail.com>
2019-01-06 19:55:24 -0200

    BFS algorithm, Level order problem C++/Kotlin

---
+++ algorithms/C++/bfs.cpp
@@ -0,0 +1,98 @@
+//Breadth-first search (BFS) is an algorithm for traversing or
+//searching tree or graph data structures.
+//Time Complexity: O(V+E)
+//Space Complexity: O(V+E)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Graph{
+private:
+  vector<vector<int>> adjList;
+public:
+
+  Graph(){}
+
+  Graph(int vertexes){
+    adjList = vector<vector<int>> (vertexes);
+  }
+
+  void addEdge(int start, int end){
+    adjList[start].push_back(end);
+  }
+
+  vector<int> getNeighbors(const int &vertex){
+    return adjList[vertex];
+  }
+
+  void printGraph(){
+    for(int i=0;i<adjList.size();i++){
+      cout<<i<<": ";
+      for(int j=0;j<adjList[i].size();j++){
+        cout<<adjList[i][j]<<"->";
+      }
+      cout<<"NULL"<<endl;
+    }
+  }
+};
+
+class BFS{
+private:
+  Graph graph;
+public:
+  BFS(Graph g){
+    graph = g;
+  }
+
+  vector<int> bfsFrom(const int &src){
+    unordered_set<int> visited;
+    queue<int> to_walk;
+    vector<int> to_return;
+
+    to_walk.push(src);
+
+    while(!to_walk.empty()){
+      int actual = to_walk.front();
+      to_walk.pop();
+
+      if(visited.find(actual)==visited.end()){
+        to_return.push_back(actual);
+        visited.insert(actual);
+
+        vector<int> neighbors = graph.getNeighbors(actual);
+        for(auto neighbor : neighbors){
+          to_walk.push(neighbor);
+        }
+      }
+    }
+
+    return to_return;
+  }
+};
+
+void printVec(const vector<int> &vec){
+  for(auto val : vec){
+    cout<<val<<" ";
+  }
+  cout<<endl;
+}
+
+int main(){
+  Graph graph(4);
+
+  graph.addEdge(0, 1);
+  graph.addEdge(0, 2);
+  graph.addEdge(1, 2);
+  graph.addEdge(2, 0);
+  graph.addEdge(2, 3);
+  graph.addEdge(3, 3);
+
+  graph.printGraph();
+
+  BFS bfs(graph);
+  printVec(bfs.bfsFrom(1));
+
+  return 0;
+
+}
---
+++ problems/C++/36_level_order.cpp
@@ -0,0 +1,67 @@
+// Problem Statement
+// Given a binary tree, return the level order traversal of its nodes' values.
+// (ie, from left to right, level by level).
+//
+// For example:
+// Given binary tree [3,9,20,null,null,15,7],
+//     3
+//    / \
+//   9  20
+//     /  \
+//    15   7
+// return its level order traversal as:
+// [
+//   [3],
+//   [9,20],
+//   [15,7]
+// ]
+
+class Solution {
+public:
+
+    vector<vector<int>> levelOrderRecursive(TreeNode* root) {
+        vector<vector<int>> result;
+        helperRecursive(root,result,0);
+        return result;
+    }
+
+    void helperRecursive(TreeNode* node, vector<vector<int>> &result, int depth){
+        if(!node)
+            return;
+
+        if(depth>=result.size()){
+            vector<int> level;
+            result.push_back(level);
+        }
+
+        result[depth].push_back(node->val);
+        helperRecursive(node->left,result,depth+1);
+        helperRecursive(node->right,result,depth+1);
+    }
+
+    vector<vector<int>> levelOrderIterative(TreeNode* root) {
+        vector<vector<int>> result;
+        if(!root)
+            return result;
+
+        queue<TreeNode*> bfs;
+        bfs.push(root);
+
+        while(!bfs.empty()){
+            vector<int> level;
+            int tam = bfs.size();
+            for(int i=0;i<tam;i++){
+                TreeNode *actual = bfs.front();
+                bfs.pop();
+                level.push_back(actual->val);
+                if(actual->left)
+                    bfs.push(actual->left);
+                if(actual->right)
+                    bfs.push(actual->right);
+            }
+            result.push_back(level);
+        }
+
+        return result;
+    }
+};
