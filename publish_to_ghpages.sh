cc79fe3bc4731bc3d3a472704c473780e7952dda
Walter <walterjgsp@gmail.com>
2019-02-15 22:57:05 -0200

    Theme aplied and automatic deploy script

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -27,4 +27,4 @@ mv public/* ../public/
 cd ..
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push origin gh-pages
