86d4a0defb62d550b3e421951d52b8f378e52641
Walter <walterjgsp@gmail.com>
2019-05-20 23:21:39 -0300

    Rename TrieNode

--- structures/C++/trie.cpp
+++ structures/C++/trie.cpp
@@ -4,33 +4,33 @@ using namespace std;
 
 class Trie{
 private:
-	class TrieNode{
+	class TNode{
 		public:
-			vector<TrieNode*> childs;
+			vector<TNode*> children;
 			bool endWord;
-			TrieNode(){
-				childs = vector<TrieNode*> (26,NULL);
+			TNode(){
+				children = vector<TNode*> (26,NULL);
 				endWord = false;
 			}
 		};
 
-	TrieNode *root;
+	TNode *root;
 
 public:
 	Trie(){
-		root = new TrieNode();
+		root = new TNode();
 	}
 
 	void insert(const string &word){
 		int index = 0;
-		TrieNode* it = root;
+		TNode* it = root;
 
 		while(index<word.size()){
 			int pos = word[index]-'a';
-			if(!it->childs[pos])
-				it->childs[pos] = new TrieNode();
+			if(!it->children[pos])
+				it->children[pos] = new TNode();
 
-			it = it->childs[pos];
+			it = it->children[pos];
 			index++;
 		}
 
@@ -39,13 +39,13 @@ public:
 
 	bool query(const string &word){
 		int index = 0;
-		TrieNode *it = root;
+		TNode *it = root;
 
 		while(index<word.size()){
 			int pos = word[index]-'a';
-			if(!it->childs[pos])
+			if(!it->children[pos])
 				return false;
-			it = it->childs[pos];
+			it = it->children[pos];
 			index++;
 		}
 
@@ -54,24 +54,24 @@ public:
 
 	void printWords(){
 			string temp;
-			TrieNode *it = root;
-			for(int i=0;i<it->childs.size();i++){
-				if(it->childs[i]){
+			TNode *it = root;
+			for(int i=0;i<it->children.size();i++){
+				if(it->children[i]){
 					temp.push_back(i+'a');
-					printWord(temp,it->childs[i]);
+					printWord(temp,it->children[i]);
 					temp.pop_back();
 				}
 			}
 	}
 
-	void printWord(string word,TrieNode *node){
+	void printWord(string word,TNode *node){
 		if(node->endWord)
 			cout<<word<<endl;
 
-		for(int i=0;i<node->childs.size();i++){
-			if(node->childs[i]){
+		for(int i=0;i<node->children.size();i++){
+			if(node->children[i]){
 				word.push_back(i+'a');
-				printWord(word,node->childs[i]);
+				printWord(word,node->children[i]);
 				word.pop_back();
 			}
 		}
