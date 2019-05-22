e69085cccf600521f3274944d4441b3a93bbe0c4
Walter <walterjgsp@gmail.com>
2019-05-22 20:39:08 -0300

    Prim implementation refactor

--- algorithms/C++/prim.cpp
+++ algorithms/C++/prim.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+typedef tuple<int,int,int> triple_t;
+
 class Graph{
 private:
   vector<vector<pair<int,int>>> adjList;
@@ -51,57 +53,63 @@ private:
 public:
   Graph applyPrim(Graph &graph){
     included_vertexes.clear();
-    priority_queue< pair<int,pair<int,int>> ,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> > pq;
-    vector<pair<int,pair<int,int>>> edgesToGraph;
-    included_vertexes.insert(0);
-    auto adjList = graph.getGraphAdjList();
+    priority_queue<triple_t,vector<triple_t>, greater<triple_t>> pq;
+    vector<triple_t> edgesToGraph;
+    int currWeight,currInit,currEnd;
 
-    for(auto edges : adjList[0]){
-      pq.push({edges.second,{0,edges.first}});
+    for(auto edge : graph.getNeighbors(0)){
+      pq.push(make_tuple(edge.second,0,edge.first));
     }
+    included_vertexes.insert(0);
 
     while(!pq.empty() && included_vertexes.size()<graph.getSize()){
-      auto current = pq.top();
+      tie(currWeight,currInit,currEnd) = pq.top();
       pq.pop();
 
-      if(included_vertexes.find(current.second.first)==included_vertexes.end() ||
-         included_vertexes.find(current.second.second)==included_vertexes.end()){
+      if(included_vertexes.find(currEnd)==included_vertexes.end()){
+        included_vertexes.insert(currEnd);
 
-           edgesToGraph.push_back({current.second.first,{current.second.second,current.first}});
-
-           for(auto edges : adjList[current.second.second]){
-             if(included_vertexes.find(edges.first)==included_vertexes.end())
-              pq.push({edges.second,{current.second.second,edges.first}});
-           }
-
-           included_vertexes.insert(current.second.first);
-           included_vertexes.insert(current.second.second);
-         }
+        edgesToGraph.push_back(make_tuple(currInit,currEnd,currWeight));
+        for(auto edge : graph.getNeighbors(currEnd)){
+          if(included_vertexes.find(edge.first)==included_vertexes.end()){
+            pq.push(make_tuple(edge.second,currEnd,edge.first));
+          }
+        }
+      }
     }
 
-    Graph minSPT(graph.getSize());
-    for(auto edge : edgesToGraph){
-      minSPT.addEdge(edge.first,edge.second.first,edge.second.second);
+    Graph minST = Graph(graph.getSize());
+    for(auto it : edgesToGraph){
+      tie(currInit,currEnd,currWeight) = it;
+      minST.addEdge(currInit,currEnd,currWeight);
     }
 
-    return minSPT;
+
+    return minST;
   }
 };
 
 int main(){
-  Graph graph(7);
+  // Graph graph(7);
 
-  graph.addEdge(0,3,5);
-  graph.addEdge(0,1,7);
-  graph.addEdge(1,2,8);
-  graph.addEdge(1,3,9);
-  graph.addEdge(1,4,7);
-  graph.addEdge(2,4,5);
-  graph.addEdge(3,4,15);
-  graph.addEdge(3,5,6);
-  graph.addEdge(4,5,8);
-  graph.addEdge(4,6,9);
-  graph.addEdge(5,6,11);
+  // graph.addEdge(0,3,5);
+  // graph.addEdge(0,1,7);
+  // graph.addEdge(1,2,8);
+  // graph.addEdge(1,3,9);
+  // graph.addEdge(1,4,7);
+  // graph.addEdge(2,4,5);
+  // graph.addEdge(3,4,15);
+  // graph.addEdge(3,5,6);
+  // graph.addEdge(4,5,8);
+  // graph.addEdge(4,6,9);
+  // graph.addEdge(5,6,11);
+  Graph graph(4);
+  graph.addEdge(0,1,1);
+  graph.addEdge(2,1,150);
+  graph.addEdge(3,2,99);
+  graph.addEdge(0,3,100);
+  graph.addEdge(2,0,200);
+
   graph.printGraph();
 
   Prim prim;
