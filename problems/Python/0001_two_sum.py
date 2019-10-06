b5d48c59308a4b0de6407fea3ccf03842ed25d18
Raul Santana <raulhsantana@gmail.com>
2019-10-06 10:04:06 -0300

    Refactor two sum python solution

--- problems/Python/0001_two_sum.py
+++ problems/Python/0001_two_sum.py
@@ -1,8 +1,14 @@
+# Problem Statement
+#
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
 from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    @classmethod
+    def two_sum(cls, nums: List[int], target: int) -> List[int]:
         known_items = {}
         for index, item in enumerate(nums):
             try:
@@ -13,8 +19,7 @@ class Solution:
         return [0, 0]
 
 
-if __name__ == "__main__":
-    nums = [2, 7, 11, 15]
-    target = 9
-    solution = Solution()
-    print(str(solution.twoSum(nums, target)))
+if __name__ == '__main__':
+    input_nums = [2, 7, 11, 15]
+    input_target = 9
+    print(str(Solution.two_sum(input_nums, input_target)))
