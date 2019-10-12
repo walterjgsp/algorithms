78177ea331957deea6649b91f1c5daa3c55d420c
Walter <walterjgsp@gmail.com>
2019-10-12 14:16:15 -0300

    First working version of test folder

---
+++ problems/C++/0001_two_sum.h
@@ -0,0 +1,11 @@
+#ifndef TWO_SUM_H_INCLUDED
+#define TWO_SUM_H_INCLUDED
+
+#include <bits/stdc++.h>
+#include "0001_two_sum.cpp"
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target);
+
+#endif
---
+++ tests/problems/C++/0001_two_sum_test.cpp
@@ -0,0 +1,16 @@
+#include "../../../problems/C++/0001_two_sum.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printVec(vector<int> vec){
+  for(auto val : vec)
+    cout<<val<<" ";
+  cout<<endl;
+}
+
+int main(){
+  vector<int> vec = {2, 11, 7, 15};
+  printVec(twoSum(vec,9));
+  return 0;
+}
