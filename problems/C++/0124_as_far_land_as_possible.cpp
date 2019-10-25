6645822557e47fbcb4fc98a36d6b552a3773f443
Walter <walterjgsp@gmail.com>
2019-10-25 08:08:16 -0300

    Refactor code problem 0124

--- problems/C++/0124_as_far_land_as_possible.cpp
+++ problems/C++/0124_as_far_land_as_possible.cpp
@@ -12,6 +12,9 @@ using namespace std;
 
 class Solution {
 private:
+    pair<int,int> inf = {INT_MAX,INT_MAX};
+    int result = -1;
+    
     vector<pair<int,int>> dirs = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
     
     bool isValidDir(const int&i, const int&j, const int &rows, const int &cols){
@@ -21,35 +24,37 @@ private:
     int calcManhattan(const int&x1, const int &y1, const int&x2, const int &y2){
         return abs(x1-x2)+abs(y1-y2);
     }
-public:
-    int maxDistance(vector<vector<int>>& grid) {
-        pair<int,int> inf = {INT_MAX,INT_MAX};
-        int rows = grid.size(),cols = grid[0].size();
-        vector<vector<pair<int,int>>> memo(rows,vector<pair<int,int>>(cols,make_pair(INT_MAX,INT_MAX)));
-        int result = -1;
-        
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==1){
-                    memo[i][j]=make_pair(i,j);
-                    result = max(result,0);
-                }else{
-                    for(auto dir : dirs){
-                        int next_i = i+dir.first, next_j = j+dir.second;
-                        if(isValidDir(next_i,next_j,rows,cols) && memo[next_i][next_j]!=inf){
-                            if(memo[i][j]==inf){
-                                memo[i][j]=memo[next_i][next_j];
-                            }else{
-                                auto old_cell = memo[i][j], new_cell = memo[next_i][next_j];
-                                int new_dist = calcManhattan(i,j,new_cell.first,new_cell.second);
-                                int old_dist = calcManhattan(i,j,old_cell.first,old_cell.second);
-                                if(new_dist<old_dist){
-                                    memo[i][j] = memo[next_i][next_j];
-                                }
-                            }
+
+    void evaluatePosition(const vector<vector<int>> &grid, vector<vector<pair<int,int>>> &memo, int i, int j, const int&rows, const int &cols){
+        if(grid[i][j]==1){
+            memo[i][j]=make_pair(i,j);
+            result = max(result,0);
+        }else{
+            for(auto dir : dirs){
+                int next_i = i+dir.first, next_j = j+dir.second;
+                if(isValidDir(next_i,next_j,rows,cols) && memo[next_i][next_j]!=inf){
+                    if(memo[i][j]==inf){
+                        memo[i][j]=memo[next_i][next_j];
+                    }else{
+                        auto old_cell = memo[i][j], new_cell = memo[next_i][next_j];
+                        int new_dist = calcManhattan(i,j,new_cell.first,new_cell.second);
+                        int old_dist = calcManhattan(i,j,old_cell.first,old_cell.second);
+                        if(new_dist<old_dist){
+                            memo[i][j] = memo[next_i][next_j];
                         }
                     }
                 }
+            }
+        }
+    }
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        int rows = grid.size(),cols = grid[0].size();
+        vector<vector<pair<int,int>>> memo(rows,vector<pair<int,int>>(cols,make_pair(INT_MAX,INT_MAX)));
+        
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                evaluatePosition(grid,memo,i,j,rows,cols);
             }
         }
         
@@ -57,26 +62,7 @@ public:
         
         for(int i=rows-1;i>=0;i--){
             for(int j=cols-1;j>=0;j--){
-                if(grid[i][j]==1){
-                    memo[i][j]=make_pair(i,j);
-                    result = max(result,0);
-                }else{
-                    for(auto dir : dirs){
-                        int next_i = i+dir.first, next_j = j+dir.second;
-                        if(isValidDir(next_i,next_j,rows,cols) && memo[next_i][next_j]!=inf){
-                            if(memo[i][j]==inf){
-                                memo[i][j]=memo[next_i][next_j];
-                            }else{
-                                auto old_cell = memo[i][j], new_cell = memo[next_i][next_j];
-                                int new_dist = calcManhattan(i,j,new_cell.first,new_cell.second);
-                                int old_dist = calcManhattan(i,j,old_cell.first,old_cell.second);
-                                if(new_dist<old_dist){
-                                    memo[i][j] = memo[next_i][next_j];
-                                }
-                            }
-                        }
-                    }
-                }
+                evaluatePosition(grid,memo,i,j,rows,cols);
             }
         }
         
