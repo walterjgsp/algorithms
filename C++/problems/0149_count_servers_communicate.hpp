1b5fb8f65d439ab8d7df02802f452e07ff13dc4c
Walter José <walterjgsp@gmail.com>
2019-11-24 18:21:41 -0300

    Count Servers that Communicate problem C++

---
+++ C++/problems/0149_count_servers_communicate.hpp
@@ -0,0 +1,14 @@
+#ifndef COUNT_SERVERS_COMMUNICATE_HPP_INCLUDED
+#define COUNT_SERVERS_COMMUNICATE_HPP_INCLUDED
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid);
+};
+
+#include "0149_count_servers_communicate.cpp"
+
+#endif
