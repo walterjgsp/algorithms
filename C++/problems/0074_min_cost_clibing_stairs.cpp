20eb10c9221222725642ae3aa310344eae782439
Walter José <walterjgsp@gmail.com>
2019-12-27 15:58:10 -0300

    Test Min Cost Clibing Stairs C++ problem

--- C++/problems/0074_min_cost_clibing_stairs.cpp
+++ C++/problems/0074_min_cost_clibing_stairs.cpp
@@ -4,28 +4,19 @@
 // Once you pay the cost, you can either climb one or two steps. You need to find
 // minimum cost to reach the top of the floor, and you can either start from the
 // step with index 0, or the step with index 1.
+#include "0074_min_cost_clibing_stairs.hpp"
 #include <bits/stdc++.h>
 
 using namespace std;
 
-class Solution {
-public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        cost.push_back(0);
-        vector<int> dp(cost.size(),0);
-        dp[0]=cost[0],dp[1]=cost[1];
+int Solution::minCostClimbingStairs(vector<int>& cost) {
+    cost.push_back(0);
+    vector<int> dp(cost.size(),0);
+    dp[0]=cost[0],dp[1]=cost[1];
 
-        for(int i=2;i<dp.size();i++){
-            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
-        }
-
-        return dp.back();
+    for(int i=2;i<dp.size();i++){
+        dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
     }
-};
 
-int main(){
-  vector<int> cost = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
-  Solution sol;
-  cout<<sol.minCostClimbingStairs(cost)<<endl;
-  return 0;
-}
+    return dp.back();
+};
---
+++ C++/problems/0074_min_cost_clibing_stairs.hpp
@@ -0,0 +1,14 @@
+#ifndef MIN_COST_CLIBING_STAIRS_HPP_INCLUDED
+#define MIN_COST_CLIBING_STAIRS_HPP_INCLUDED
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost);
+};
+
+#include "0074_min_cost_clibing_stairs.cpp"
+
+#endif
