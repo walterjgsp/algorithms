afdeb937124a65c3fdc6c21bde7235d376744884
Walter José <walterjgsp@gmail.com>
2020-03-09 13:19:38 -0300

    Time Needed to Inform All Employees problem C++

---
+++ C++/problems/0162_time_needed_inform_employees.hpp
@@ -0,0 +1,14 @@
+#ifndef TIME_NEEDED_INFORM_EMPLOYEES_HPP_INCLUDED
+#define TIME_NEEDED_INFORM_EMPLOYEES_HPP_INCLUDED
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime);
+};
+
+#include "0162_time_needed_inform_employees.cpp"
+
+#endif
