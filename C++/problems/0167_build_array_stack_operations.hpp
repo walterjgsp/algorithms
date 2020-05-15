affdce09692cbe1e9308db91d66c016e8353de39
Walter José <walterjgsp@gmail.com>
2020-05-15 11:24:37 -0300

    Build Array Stack Operations problem C++

---
+++ C++/problems/0167_build_array_stack_operations.hpp
@@ -0,0 +1,15 @@
+#ifndef BUILD_ARRAY_STACK_OPERATIONS_HPP_INCLUDED
+#define BUILD_ARRAY_STACK_OPERATIONS_HPP_INCLUDED
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n);
+};
+
+#include "0167_build_array_stack_operations.cpp"
+
+#endif
