69a29f00b46bfc5e688f067cb40cb617f60c1dd7
Walter José <walterjgsp@gmail.com>
2020-05-31 23:01:49 -0300

    Backspace string compare C++ solution

---
+++ C++/problems/0168_backspace_string_compare.hpp
@@ -0,0 +1,18 @@
+#ifndef BACSPACE_STRING_COMPARE_HPP_INCLUDED
+#define BACSPACE_STRING_COMPARE_HPP_INCLUDED
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+private:
+    string buildString(const string &word);
+
+public:
+    bool backspaceCompare(string S, string T);
+};
+
+#include "0168_backspace_string_compare.cpp"
+
+#endif
---
+++ C++/tests/problems/0168_backspace_string_compare_test.cpp
@@ -0,0 +1,19 @@
+#include "../../problems/0168_backspace_string_compare.hpp"
+#include <bits/stdc++.h>
+#include "../../frameworks/catch.hpp"
+#include "../../frameworks/asserts.hpp"
+
+using namespace std;
+
+TEST_CASE( "Backspace String Compare" ) {
+    Solution sol;
+    
+    REQUIRE(sol.backspaceCompare("ab#c","ad#c"));   
+
+    REQUIRE(sol.backspaceCompare("ab##","c#d#"));   
+    
+    REQUIRE(sol.backspaceCompare("a##c","#a#c"));   
+    
+    REQUIRE(!sol.backspaceCompare("a#c","b"));   
+
+}
