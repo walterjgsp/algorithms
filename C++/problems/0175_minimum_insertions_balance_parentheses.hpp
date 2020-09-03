0603def314770b40dc1ccd44a38f7bdc372d00d4
Walter José <walterjgsp@gmail.com>
2020-09-03 07:36:10 -0300

    Minimum insertions to balance parentheses

---
+++ C++/problems/0175_minimum_insertions_balance_parentheses.hpp
@@ -0,0 +1,15 @@
+#ifndef MINIMUM_INSERTIONS_BALANCE_PARENTHESES_HPP
+#define MINIMUM_INSERTIONS_BALANCE_PARENTHESES_HPP
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minInsertions(string s);
+};
+
+#include "0175_minimum_insertions_balance_parentheses.cpp"
+
+#endif
