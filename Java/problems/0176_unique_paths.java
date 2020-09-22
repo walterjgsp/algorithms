9992494c31a0405b477a55bd0c59ee18af555d24
raulhsant <raulhsantana@gmail.com>
2020-09-22 00:21:31 -0300

    fix spacing

--- Java/problems/0176_unique_paths.java
+++ Java/problems/0176_unique_paths.java
@@ -7,36 +7,36 @@
 
 // How many possible unique paths are there?
 
-class Solution{
+class Solution {
     public int uniquePaths(int m, int n) {
-        int [][] grid = new int[m][n];
+        int[][] grid = new int[m][n];
         fillFirstCases(grid);
         fillNextCases(grid);
-        return grid[m-1][n-1];
+        return grid[m - 1][n - 1];
     }
 
-    private void fillFirstCases(int[][] grid){
-        for(int i=0; i<grid.length; i++){
+    private void fillFirstCases(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
             grid[i][0] = 1;
         }
-        for(int i=0; i<grid[0].length; i++){
+        for (int i = 0; i < grid[0].length; i++) {
             grid[0][i] = 1;
         }
     }
 
-    private void fillNextCases(int[][] grid){
-        for(int i=1; i<grid.length; i++){
-            for(int j=1; j<grid[i].length; j++){
-                int top = grid[i-1][j];
-                int left = grid[i][j-1];
-                grid[i][j] = top+left;
+    private void fillNextCases(int[][] grid) {
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[i].length; j++) {
+                int top = grid[i - 1][j];
+                int left = grid[i][j - 1];
+                grid[i][j] = top + left;
             }
         }
     }
 
 }
 
-class Main{
+class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
         int m = 3;
