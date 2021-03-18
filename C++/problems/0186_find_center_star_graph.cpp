c036d3012e0407a5e866bef9cb35d74a4187aa46
Walter <>
2021-03-18 09:14:42 -0300

    Find center of star graph

---
+++ C++/problems/0186_find_center_star_graph.cpp
@@ -0,0 +1,28 @@
+//Problem Statement
+// There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a 
+// graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
+
+// You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates 
+// that there is an edge between the nodes ui and vi. Return the center of the given star graph.
+
+#include "0186_find_center_star_graph.hpp"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> Solution::findCenter(vector<vector<int>>& edges){
+    vector<int> vertex(edges.size()+1);
+        
+    for(auto e : edges){
+        vertex[e[0]-1]++;
+        vertex[e[1]-1]++;
+    }
+    
+    for(int i=0;i<vertex.size();i++){
+        if(vertex[i]>1){
+            return i+1;
+        }
+    }
+    
+    return 0;
+}
---
+++ C++/problems/0186_find_center_star_graph.hpp
@@ -0,0 +1,15 @@
+#ifndef FIND_CENTER_STAR_GRAPH_HPP_INCLUDED
+#define FIND_CENTER_STAR_GRAPH_HPP_INCLUDED
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges);
+};
+
+#include "0186_find_center_star_graph.cpp"
+
+#endif
