7501179aed9db3541cf9ae69b356451f3e1a0536
raulhsant <raulhsantana@gmail.com>
2022-03-30 20:28:25 -0300

    Add search 2d matrix solution

---
+++ Python/problems/problem_0193_seach_2d_matrix.py
@@ -0,0 +1,30 @@
+# Problem Statement
+
+# Write an efficient algorithm that searches for a value target in an m x n integer matrix.
+# This matrix has the following properties:
+#   Integers in each row are sorted from left to right.
+#   The first integer of each row is greater than the last integer of the previous row.
+from typing import List
+
+
+class Solution:
+
+    @staticmethod
+    def searchMatrix(matrix: List[List[int]], target: int) -> bool:
+
+        if matrix[0][0] == target:
+            return True
+
+        # if the first item is higher than target, then target is not feasible
+        if matrix[0][0] > target:
+            return False
+
+        # walk throught the first line
+        for i in range(1, len(matrix)):
+            if matrix[i][0] == target:
+                return True
+            if matrix[i][0] > target:
+                return target in matrix[i-1]
+
+        # check if it is the last column
+        return target in matrix[len(matrix)-1]
